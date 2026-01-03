1064d5eec360ee1e856837824b7b3971261f27e3
arpitkesharwani77 <arpitkesharwani77@gmail.com>
2026-01-03 16:15:21 +0530

    cors fix , frontend lib & setup home page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SignUpPage from "./pages/SignUpPage";
+import LoginPage from "./pages/LoginPage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import { useAuthStore } from "./store/useAuthStore";
+import { Loader } from "lucide-react";
 
 const App = () => {
-  return <div className="text-red-500">App</div>;
+  const { authUser, checkAuth, isChekingAuth } = useAuthStore();
+
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
+  console.log(authUser);
+
+  if (isChekingAuth && !authUser)
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <Loader className="size-10 animate-spin" />
+      </div>
+    );
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={authUser?<HomePage />:<Navigate to="/login"/>}></Route>
+        <Route path="/signup" element={!authUser?<SignUpPage />:<Navigate to="/"/>}></Route>
+        <Route path="/login" element={!authUser?<LoginPage />:<Navigate to="/" />}></Route>
+        <Route path="/settings" element={<SettingsPage />}></Route>
+        <Route path="/profile" element={authUser? <ProfilePage />:<Navigate to="/login"/>}></Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
