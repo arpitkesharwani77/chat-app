37542c3dccaaa8ec4c2f84fdb199ba7b370c0f66
arpitkesharwani77 <arpitkesharwani77@gmail.com>
2026-01-04 22:33:42 +0530

    add setting page and themes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,11 +7,13 @@ import LoginPage from "./pages/LoginPage";
 import SettingsPage from "./pages/SettingsPage";
 import ProfilePage from "./pages/ProfilePage";
 import { useAuthStore } from "./store/useAuthStore";
+import { useThemeStore } from "./store/useThemeStore";
 import { Loader } from "lucide-react";
 import { Toaster } from "react-hot-toast";
 
 const App = () => {
   const { authUser, checkAuth, isChekingAuth } = useAuthStore();
+  const { theme } = useThemeStore();
 
   useEffect(() => {
     checkAuth();
@@ -26,7 +28,7 @@ const App = () => {
       </div>
     );
   return (
-    <div>
+    <div data-theme={theme}>
       <Navbar />
 
       <Routes>
